94ea8c2d0f6de217f33b1e0d686bff5b53b160ac
Ghazanfar Mehmood <ghazanfar4154@gmail.com>
2022-08-23 18:32:54 +0500

    file_sharing

---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+import 'package:image_picker/image_picker.dart';
+import 'package:share/share.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.brown,
+      ),
+      themeMode: ThemeMode.dark,
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController text = TextEditingController();
+
+  void _incrementCounter() {
+    setState(() {
+      // Share.share(text.text);
+    });
+    text.clear();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(
+                  hintText: " Enter Text For Sharing", label: Text("Text")),
+              controller: text,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.share),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
